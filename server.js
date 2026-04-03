7d438c7c0585272b9b6ddc96ba1f9530d2554a38
Henrique-Linhares <hlinhares453@gmail.com>
2026-04-03 11:59:26 -0300

    Criando endpoints de GET e POST

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/users', (req, res) => {
+    users.push(req.body);
+    res.send("Usuário cadastrado com sucesso!");
+});
+
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+
+app.listen(3000)
